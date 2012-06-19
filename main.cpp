1233c360a88a2ed5270d43ed0e9cd73faca72fd2
Pradana Aditya Laksana <Heri@Chokiez-PC.(none)>
2012-06-19 13:36:16 +0700

    Pradana Ganteng

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <OpenGL/OpenGL.h>
 #include <GLUT/glut.h>
 #else
-#include <glut.h>
+#include <GL/glut.h>
 #include <GL/glu.h>
 #include <GL/gl.h>
 #include "imageloader.h"
@@ -323,7 +323,7 @@ void display(void) {
 	glPushMatrix();
 
 	//glBindTexture(GL_TEXTURE_3D, texture[0]);
-	drawSceneTanah(_terrainAir, 0.0f, 0.2f, 0.5f);
+	drawSceneTanah(_terrainAir, 0.1f, 0.0f, 0.1f);
 	glPopMatrix();
 	
 //	glMatrixMode(GL_MODELVIEW);
@@ -337,7 +337,7 @@ cylinder: untuk membentuk model tabung
                   
 */                
                   
-glTranslatef(0,0,-50);// besar kecilnya mobil
+glTranslatef(0,0,50);// besar kecilnya mobil
                   
 //Loop++;         
   // glRotatef(Loop,0.9,4.0,0.6);//mobil yang berotasi
@@ -439,6 +439,118 @@ cylinder(1, 1,12);
 
 
 glPopMatrix();
+
+
+
+
+
+glTranslatef(0,0,-100);// besar kecilnya mobil
+                  
+//Loop++;         
+  // glRotatef(Loop,1.0,1.0,1.0);//mobil yang berotasi
+                  
+glPushMatrix();//body
+//glpushmatrix(); 
+glColor3f(0.0,0.1,0.0);
+blok(10, 3, 2);   
+                  
+glTranslatef(0, 9, 0);
+blok(10, 3, 2);   
+glTranslatef(10, -10,0);
+blok(10, 5, 2);   
+glRotatef(-35, 0, 0, 15);
+glTranslatef(0, 7,0);
+blok(10, 2, 2);   
+glTranslatef(2, 4.9,-2.5);
+glColor3f(0.9,0.9,0.9);// warna kaca
+blok(0.5, 20, 31);
+                  
+glRotatef(180, 45, -45, 0);
+//glTranslatef(0, 10,0);
+//blok(3, 2, 5);  
+//cylinder(2,2,30);
+//glTranslatef(-10, -10,0);
+//glRotatef(90, 45,-45, 0);
+//cylinder(2,2,30);
+//glRotatef(90, -45, 90, 90);
+//blok(5, 3, 2);  
+                  
+glPopMatrix();    
+                  
+                  
+glPushMatrix();//roda
+glColor3f(0.0,0.0,0.0);
+glTranslatef(20, -8,-7);
+cylinder(5, 5,3); 
+                  
+glColor3f(0.0,0.0,0.0);
+glTranslatef(-20, 8,7);
+glTranslatef(-5, -8,-7);
+cylinder(5, 5,3); 
+                  
+glColor3f(0.0,0.0,0.0);
+glTranslatef(5, 8,7);
+glRotatef(180,0,180,0);
+glTranslatef(3,-8,-17);
+cylinder(5, 5,3); 
+                  
+glColor3f(0.0,0.0,0.0);
+glTranslatef(-3,8,17);
+glTranslatef(-22,-8,-17);
+cylinder(5, 5,3); 
+                  
+glColor3f(1.0,1.0,1.0);
+                  
+glRotatef(90,1,0,0);
+glTranslatef(8, 2.5,-15);
+blok(2, 4, 5);    
+                  
+                  
+glRotatef(90,0,1,0);
+glTranslatef(0,-0.2,7);
+blok(2, 4, 8);    
+                  
+                  
+glRotatef(0,0,0,0);
+glTranslatef(0,19.2,0);
+blok(2, 4, 8);    
+                  
+glRotatef(90,0,1,0);
+glTranslatef(7, 0,-8);
+blok(2, 4, 5);    
+                  
+glColor3f(6.9,9.0,0.0);//lampu
+glRotatef(90,0,1,0);
+glTranslatef(0,-3,20);
+cylinder(2, 2,3); 
+                  
+glColor3f(6.9,9.0,0.0);//lampu
+glRotatef(0,0,0,0);
+glTranslatef(0,-12,0);
+cylinder(2, 2,3); 
+                  
+glColor3f(0.0,0.0,0.0);//knalpot
+glRotatef(0,0,0,0);
+glTranslatef(0,0,-52);
+cylinder(1, 1,3); 
+                  
+glColor3f(1.0,1.0,1.0);
+glRotatef(90,1,0,0);
+glTranslatef(-8,3.5,-12);
+blok(2, 4, 8);    
+                  
+glColor3f(9.0,0.0,0.0);//lampu atas
+glRotatef(0,0,0,0);
+glTranslatef(-8,28,0);
+cylinder(1, 1,12);
+
+
+glPopMatrix();
+
+
+
+
+
 glutSwapBuffers();
 
 	glFlush();
